ab0bafdac31bd0048ac851853a63506781004134
Danilo Monteiro <danilomonteiroo@gmail.com>
2021-09-05 11:28:58 -0300

    Bootstrap ok
    bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,16 @@
 import numpy as np
 class Bootstrap:
 
-    def __init__(self) -> None:
+    def __init__(self):
         pass
-
-    def doBoot(self, dados):
-        sample_mean = []
-        for _ in range(1000):  #so B=10000
-            sample_n = np.random.choice(dados, size=100)
-            sample_mean.append(sample_n.mean())
-        return sample_mean
     def soma(self):
         return 1 + 2
+
+    def doBoot(self, dados, tamanho):
+        sample_mean = []
+        for _ in range(1000):  #so B=10000
+            sample_n = np.random.choice(dados, size=tamanho)
+            sample_mean.append(sample_n.mean())
+        return sample_mean
+
    
